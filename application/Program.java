9201ac583bed8961c5342194d7627bc82f0dba43
Thalles Rocha <thalles_leopoldino@outlook.com>
2023-06-06 14:51:36 -0300

    Classe position criada

--- application/Program.java
+++ application/Program.java
@@ -1,14 +1,13 @@
 package application;
 
+
+
 public class Program {
-
-	public Program() {
-		// TODO Auto-generated constructor stub
-	}
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+		
 	}
 
 }
